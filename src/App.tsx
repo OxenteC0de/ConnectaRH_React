c2c4a621aa212e22302d8000aa2328821d8c6932
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-11-05 08:01:38 -0300

    Home e Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
 
-function App() {
-  return <></>;
+import Sobre from './pages/sobre/Sobre'
+import Produtos from './pages/produtos/Produtos'
+
+function App() { //Por que não consigo comentar dentro da função? De qualquer forma, Navbar, sobre, footer e produtos estão assim pq não importei
+  return (
+    <>
+      <BrowserRouter>
+        //<Navbar /> 
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/produtos" element={<Produtos />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
